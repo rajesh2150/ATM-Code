6adf293f5fa560a74183402e0d5e265563503763
Rajesh Korlapati <77844866+rajesh2150@users.noreply.github.com>
2022-08-05 15:19:52 +0530

    Update upi.py

--- upi.py
+++ upi.py
@@ -1,55 +1,57 @@
-pin=int(0000);#pin number
-myname='rajesh' #user name
-user_name=input("Enter your name :")
-user_pin=int(input("Enter Your pin :"));
-balance=int(10000);
-if pin==user_pin and myname==user_name: #validation
-    print('')
-    print("Welocome",user_name)
-    option=int(input(" 1.Balance 2.Send :"));
-    if option==2:
-        amount=int(input("Enter Amount to Send :"))
-        if amount>balance:
-                 print('-----------------------------------------')
-                 print('You Entered Wromg Amount and Enter correctly')
-                 print('-----------------------------------------')
-
-        elif amount==0:
-                print('-----------------------------------------')
-                print("Amount Must Be Greater Than zero")
-                print('-----------------------------------------')
-
-        elif amount<=0:
-            print('-----------------------------------------')
-            print(' Negative Amount Not Valid ')
-            print('-----------------------------------------')
-
-       # c=balance-amount
-        else:
-            PIN1=int(input("Enter Your PIN:"))
-            if PIN1==pin:
-                 c=balance-amount
-                 print('---------------------------------------------')
-                 print("Your amount ",amount,"Debited Successfully")
-                 print('---------------------------------------------')
-
-                 print("Your Remaing balance is:",c);
-        #elif amount>=balance:
-                #print('You Entered Wromg Amount and Enter correctly')
-       # elif amount==0:
-                #print("Enter Greater Than zero")
-            else:
-               print('---------------------------------------------------')
-               print("Entered PIN is Wrong And Enter A Correct PIN")
-               print('---------------------------------------------------')
-    
-    else:
+def upi_code():
+    pin=int(0000);#pin number
+    myname='rajesh' #user name
+    user_name=input("Enter your name :")
+    user_pin=int(input("Enter Your pin :"));
+    balance=int(10000);
+    if pin==user_pin and myname==user_name: #validation
         print('')
-        print('-----------------------------------------')
-        print("Your Balance is",balance)
-        print('-----------------------------------------')
+        print("Welocome",user_name)
+        option=int(input(" 1.Balance 2.Send :"));
+        if option==2:
+            amount=int(input("Enter Amount to Send :"))
+            if amount>balance:
+                     print('-----------------------------------------')
+                     print('You Entered Wromg Amount and Enter correctly')
+                     print('-----------------------------------------')
 
-else:
-    print("Pin Not Valid")
-print('Made By Rajesh Korlapati')
-print("Thank you ")
+            elif amount==0:
+                    print('-----------------------------------------')
+                    print("Amount Must Be Greater Than zero")
+                    print('-----------------------------------------')
+
+            elif amount<=0:
+                print('-----------------------------------------')
+                print(' Negative Amount Not Valid ')
+                print('-----------------------------------------')
+
+           # c=balance-amount
+            else:
+                PIN1=int(input("Enter Your PIN:"))
+                if PIN1==pin:
+                     c=balance-amount
+                     print('---------------------------------------------')
+                     print("Your amount ",amount,"Debited Successfully")
+                     print('---------------------------------------------')
+
+                     print("Your Remaing balance is:",c);
+            #elif amount>=balance:
+                    #print('You Entered Wromg Amount and Enter correctly')
+           # elif amount==0:
+                    #print("Enter Greater Than zero")
+                else:
+                   print('---------------------------------------------------')
+                   print("Entered PIN is Wrong And Enter A Correct PIN")
+                   print('---------------------------------------------------')
+
+        else:
+            print('')
+            print('-----------------------------------------')
+            print("Your Balance is",balance)
+            print('-----------------------------------------')
+
+    else:
+        print("Pin Not Valid")
+    print('Made By Rajesh Korlapati')
+    print("Thank you ")
+upi_code()
